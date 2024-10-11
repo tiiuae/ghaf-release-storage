152b3b57e537e02efc5f6154666adee39ea237d6
karim mdmirajul <karim.mdmirajul@unikie.com>
2024-10-11 15:05:05 +0300

    added index deployment ghaf 24 09 readme added

---
+++ deployment-script/deploy_tar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+storage_account="ghafreleasesstorage"
+container_name="$web"
+target_directory="ghaf-24-09"
+source_directory="/home/karim/ghaf-infra-latest/scripts/get-artifacts-out/artifacts/ghaf-release-pipeline/build_15-commit_fd31a80bd525033532063f9034b9de0f0288d672"
+
+upload_to_storage() {
+  local file_path="$1"
+  local filename=$(basename "$file_path")
+
+  azcopy copy "$file_path" "https://${storage_account}.blob.core.windows.net/${container_name}/${target_directory}/${filename}"
+}
+
+find "$source_directory" -type f -name "*.tar" -exec upload_to_storage {} \;
---
+++ readme.md
@@ -0,0 +1,1 @@
+azcopy copy /home/karim/ghaf-infra-latest/scripts/get-artifacts-out/artifacts/ghaf-release-pipeline/build_15-commit_fd31a80bd525033532063f9034b9de0f0288d672 https://ghafreleasesstorage.blob.core.windows.net/$web/ghaf-24-09-01/
